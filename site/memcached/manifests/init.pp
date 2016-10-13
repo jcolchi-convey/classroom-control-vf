5a3466f0d060f9f199fa56d2d7c5d5f30df9e31e
jcolchi-convey <jcolchi@conveydevelopment.com>
2016-10-13 12:52:13 -0300

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,20 @@
+class memcached{
+
+file { '/etc/sysconfig/memcached.conf':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  source => 'puppet:///modules/memcached/memcached',
+  require => Package['memcached'],
+ }
+ package { 'memcached':
+  ensure => present,
+ }
+ 
+service { 'memcached':
+ ensure => running,
+ enable => true,
+ subscribe => File['/etc/sysconfig/memcached'],
+}
+}
