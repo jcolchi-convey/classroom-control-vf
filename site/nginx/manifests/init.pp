1d733150e85fa59335a1f2046a20bfa90d2819fd
jcolchi-convey <jcolchi@conveydevelopment.com>
2016-10-14 16:36:54 -0300

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,49 +1,28 @@
-class nginx {
-
-  yumrepo { 'updates':
-    ensure     => 'present',
-    descr      => 'CentOS-$releasever - Updates',
-    enabled    => '1',
-    gpgcheck   => '1',
-    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
-  }
-
-  File {
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-  }
-  
-  package { 'nginx':
-    ensure => present,
-  }
-  
-  file { [ '/var/www', '/etc/nginx/conf.d' ]:
-    ensure => directory,
-  }
-  
-  file { '/var/www/index.html':
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  
-  file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
-	require => Package['nginx'],
-	notify => Service['nginx'],
-  }
-  
-  file { '/etc/nginx/conf.d/default.conf':
-	ensure => file,
-	source => 'puppet:///modules/nginx/default.conf',
-	notify => Service['nginx'],
-	require => Package['nginx'],
-  }
-  
-  service { 'nginx':
-	ensure => running,
-	enable => true,
-  }
+class nginx (
+  $root = undef,
+) {
+  case $::osfamily {
+    'redhat','debian' : {
+        $package = 'nginx'
+	$owner = 'root'
+	$group = 'root'
+	# $docroot = '/var/www'
+	$confdir = '/etc/nginx'
+	$logdir = '/var/log/nginx'
+	# this will be used if we don't pass in a value
+	$default_docroot = '/var/www'
+	}
+    'windows' : {
+	$package = 'nginx-service'
+	$owner = 'Administrator'
+	$group = 'Administrators'
+	# $docroot = 'C:/ProgramData/nginx/html'
+	$confdir = 'C:/ProgramData/nginx'
+	$logdir = 'C:/ProgramData/nginx/logs'
+	# this will be used if we don't pass in a value
+	$default_docroot = 'C:/ProgramData/nginx/html'
+     }
+     default : {
+	fail("Module ${module_name} is not supported on ${::osfamily}")
+     }
 }
